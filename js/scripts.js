676faeb07ddeab317b31018f4d6bedb2ba154ddf
Sarah Lannigan <sarah.lannigan@gmail.com>
2014-03-25 19:30:45 -0400

    Got click draggable bowl working

--- js/scripts.js
+++ js/scripts.js
@@ -89,4 +89,71 @@ $(document).ready(function() {
 		$(this).children(".desc").toggleClass("hidden");
 	});
 
+
+	///////////////////////////////
+	//     BOWL PLACEMENT        //
+	///////////////////////////////
+
+	//from http://jsfiddle.net/Zevan/QZejF/3/
+
+	var canvas = $("#c");
+	var c = canvas[0].getContext("2d");
+
+	var path = "img/bowl-small.png";
+	var image = new DragImage(path, 75, 75);
+
+	var loop = setInterval(function() {
+
+	    c.fillStyle = "white";
+	    c.fillRect(0, 0, 200, 150);
+
+	    image.update();
+
+	}, 30);
+
+	var mouseX = 0, mouseY = 0;
+	var mousePressed = false;
+	canvas.mousemove(function(e) {
+	  mouseX = e.offsetX;
+	  mouseY = e.offsetY;
+	})
+	 
+	$(document).mousedown(function(){
+	    mousePressed = true;
+	}).mouseup(function(){
+	    mousePressed = false;
+	});
+
+	function DragImage(src, x, y) {
+	    var that = this;
+	    var startX = 0, startY = 0;
+	    var drag = false;
+	    this.x = x;
+	    this.y = y;
+	    var img = new Image();
+	    img.src = src;
+	    this.update = function() {
+	        if (mousePressed){
+	            var left = that.x;
+	            var right = that.x + img.width;
+	            var top = that.y;
+	            var bottom = that.y + img.height;
+	            if (!drag){
+	              startX = mouseX - that.x;
+	              startY = mouseY - that.y;
+	            }
+	            if (mouseX < right && mouseX > left && mouseY < bottom && mouseY > top){
+	               drag = true;
+	            }
+	        }else{
+	           drag = false;
+	        }
+	        if (drag){
+	            that.x = mouseX - startX;
+	            that.y = mouseY - startY;
+	        }
+	        c.drawImage(img, that.x, that.y);
+	    }
+	}
+
 });
